2dca182bd77f4c2f4bb1ac38b621e7051a5a70fc
olsgen3 <43924102+olsgen3@users.noreply.github.com>
2019-02-19 13:34:13 +0800

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "UTF-8" name "viewport" content = "width-device=width, initial-scale=1"/>
+		<title>OOP PHP Registrarion and Login Form </title>
+		<link rel = "stylesheet" type = "text/css" href = "login.php" />
+	</head>
+<body>
+	<nav class = "navbar navbar-default">
+		<div class = "container-fluid">
+\
+</a>		</div>
+	</nav>
+	<br />
+	<br />
+	<br />
+	<div class = "row">
+		<div class = "col-md-4">
+		</div>
+		<div class = "col-md-4 well">
+			<h4 class = "text-danger">OOP PHP Registration and Login Form Using MySQLi</h4>
+			<hr style = "border-top:1px dotted #000;"/>
+			<form method = "POST" action = "login.php">
+				<div class="form-group">
+				
+					<input type = "text" placeholder = "Username"  name = "username" class = "form-control"/>
+				</div>
+				<div class="form-group">
+					<input type = "password" placeholder = "Password"  name = "password" class = "form-control">
+			
+					
+				<button class = "btn btn-success" name = "login"><span class = "glyphicon glyphicon-log-in"></span> Login</button>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
